6faf10369789318b82eb9f6a809101a1af997c77
alexb192 <37848326+alexb192@users.noreply.github.com>
2020-04-01 01:08:17 -0400

    made a real contact-us page + css

--- loginandDB/contactus.php
+++ loginandDB/contactus.php
@@ -3,18 +3,34 @@
     include('masterpage.php');
 ?>
 
-<!Doctype HTML>
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="contactus.css" />
+
 <html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<body>
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-    testing this garbage, hey does this work?
-</body>
+    <script
+      src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"
+      referrerpolicy="origin"
+    ></script>
 
+    <script>
+      tinymce.init({
+        selector: "#mytextarea"
+      });
+    </script>
+  </head>
+
+  <body>
+    <h1>Contact Us</h1>
+    <h2>We love to hear your feedback!</h2>
+
+    <div class="tinymce">
+      <form method="post">
+        <textarea id="mytextarea">Hello, World!</textarea>
+      </form>
+    </div>
+  </body>
 </html>
