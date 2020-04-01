a0e036ada90a6402d2fde7f7f8f6618fd149bbcd
Zaid <32018888+Z8D@users.noreply.github.com>
2020-04-01 00:02:52 -0400

    login and redirect

--- loginandDB/login.php
+++ loginandDB/login.php
@@ -2,16 +2,21 @@
 require_once('common.php');
 ?>
 <!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="loginpage.css" />
+
 <html>
-<head>
-  <title>Login</title>
-</head>
-<body>
-  <h1>Login Form</h1>
-  <form action="processlogin.php" method="post">
-    <b>Username:</b> <input type="text" name="uname" /><br />
-    <b>Password:</b> <input type="password" name="pass" /><br/>
-    <input type="submit" />
-  </form>
-</body>
+  <head>
+    <title>Landing Page</title>
+  </head>
+
+  <body>
+    <div>
+      <form action="processlogin.php" method="post">
+        <b>Username:</b> <input type="text" name="uname" /><br />
+        <b>Password:</b> <input type="password" name="pass" /><br />
+        <input class="submitbutton" type="submit" value="Log In" />
+        <button>Continue as Guest</button>
+      </form>
+    </div>
+  </body>
 </html>
