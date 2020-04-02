419d4dc21c4df7b35baef6cb9f6adfccdcf53bee
ali-hazime <a.hazime@outlook.com>
2020-04-02 09:48:22 -0400

    Update mainPage.php

--- loginandDB/mainPage.php
+++ loginandDB/mainPage.php
@@ -6,16 +6,10 @@
 <!Doctype HTML>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <link rel="stylesheet" href="plannerStyle.css">
-
     <title>
     Home Page
     </title>
-
 </head>
 
 <body>
