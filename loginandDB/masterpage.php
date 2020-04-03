764e98a4ac8a9aedd3021b253e95621a38b51501
alexb192 <37848326+alexb192@users.noreply.github.com>
2020-04-03 01:21:28 -0400

    page styles
    master page style is controlled by cookiecheck.php and a php script at the start of masterpage. masertpage.php no longer has a predetermined css.

--- loginandDB/masterpage.php
+++ loginandDB/masterpage.php
@@ -1,45 +1,43 @@
 <?php
 require_once('common.php');
+include './masterpage_cookiecheck.php';
 ?>
 
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" type="text/css" href="masterpagestyle.css" />
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="masterpagestyle_<?php echo $styleChoice ?>.css"
+  />
 
+  <div>
+    <nav>
+      <ul id="navbaritems" class="navbaritems">
+        <li id="navbarli" class="navbarli"><a href="mainPage.php">Home</a></li>
+        <li id="navbarli" class="navbarli">
+          <a href="viewcalendar.php">View Calendar</a>
+        </li>
+        <li id="navbarli" class="navbarli"><a href="">Settings</a></li>
+        <?php if (!UserUtils::is_logged_in()) { ?>
+        <li id="navbarli" class="navbarli"><a href="login.php">Log In</a></li>
+        <?php } else { ?>
+        <li id="navbarli" class="navbarli"><a href="logout.php">Log Out</a></li>
+        <?php } ?>
+      </ul>
+    </nav>
+  </div>
 
+  <body></body>
 
-<div>
-  <nav>
-    <ul id="navbaritems" class="navbaritems">
-      <li id="navbarli" class="navbarli"><a href="mainPage.php">Home</a></li>
-      <li id="navbarli" class="navbarli"><a href="viewcalendar.php">View Calendar</a></li>
-      <li id="navbarli" class="navbarli"><a href="">Settings</a></li>
-      <?php if (!UserUtils::is_logged_in()) { ?>
-  <li id="navbarli" class="navbarli"><a href="login.php">Log In</a></li>
-<?php } else { ?>
-  <li id="navbarli" class="navbarli"><a href="logout.php">Log Out</a></li>
-<?php } ?>
-      
-    </ul>
-  </nav>
-</div>
-
-<body>
-
-</body>
-
-<div>
-
-<footer id="footer"> 
-
-  <nav>
-    <ul class="footeritems">
-      <li>CS-3340 Group Project</li>
-      <li><a href="contactus.php">Contact Us</a></li>
-    </ul>
-  </nav>
-
-</footer>  
-</div>
+  <div>
+    <footer id="footer">
+      <nav>
+        <ul class="footeritems">
+          <li>CS-3340 Group Project</li>
+          <li><a href="contactus.php">Contact Us</a></li>
+        </ul>
+      </nav>
+    </footer>
+  </div>
 </html>
-
