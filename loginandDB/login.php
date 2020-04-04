6d0b027943a8ca1009963b41152d176950a59bd4
Zaid <32018888+Z8D@users.noreply.github.com>
2020-04-03 23:51:44 -0400

    login.php restored

--- loginandDB/login.php
+++ loginandDB/login.php
@@ -12,6 +12,18 @@ $y =  $_SESSION['var'];
 <html>
   <head>
     <title>Login</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("input").focus(function(){
+    $(this).css("background-color", "yellow");
+  });
+  $("input").blur(function(){
+    $(this).css("background-color", "green");
+  });
+});
+</script>
   </head>
 
   <body>
