c9ae5c3b0a8ec5064ebf76070d7521cea612c3dd
alexb192 <37848326+alexb192@users.noreply.github.com>
2020-04-07 04:34:41 -0400

    maybe this will work

--- loginandDB/viewtickets.php
+++ loginandDB/viewtickets.php
@@ -13,19 +13,34 @@ if (!UserUtils::is_admin())
 
 <html>
     <body>
-        <table>
+        <table id="ticketTable">
                     <tr>
                         <th>TicketID</th>
                         <th>Username</th>
                         <th>Content</th>
                     </tr>
             <?php
+                $counter = 0;
                 $db = new DBTickets();
                 $everything = $db->lookup_all();
 
                 foreach ($everything as $var)
-                    echo "<tr><td>$var[0]</td><td>$var[1]</td><td>$var[2]</td></tr>"; 
+                {
+                    echo "<tr id="$counter"><td>$var[0]</td><td>$var[1]</td><td>$var[2]</td><td><button onclick="deleteRow($counter)">Delete</button></td></tr>";
+                    $counter++;
+                }
             ?>
         </table>
     </body>
 </html>
+
+<script>
+    function deleteRow(var rowNumber) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("ticketTable").deleteRow(rowNumber);
+            }
+        };
+    }
+</script>
